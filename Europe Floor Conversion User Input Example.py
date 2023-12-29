d5fbd1cd5c340d38cbaef8884cd2e714468e631d
Shawn Releford <74284733+spr2402@users.noreply.github.com>
2023-12-29 15:20:15 -0600

    Add files via upload

---
+++ Europe Floor Conversion User Input Example.py
@@ -0,0 +1,7 @@
+inp = input('Europe floor?')
+usf = int(inp) + 1
+print('US floor', usf)
+# User input example
+# Python for Everybody code challenge
+# Input Europe Floor # and adds 1 to get US Floor Number
+
