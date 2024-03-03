784acac3f8d1479e3b6c429d979c4e8c66a22054
Shawn Releford <74284733+spr2402@users.noreply.github.com>
2024-03-03 17:08:35 -0600

    Add files via upload
    Learning about string Methods. Decided to try PyCharm community edition vs the usual Vscode.

---
+++ String Methods.py
@@ -0,0 +1,13 @@
+course = 'Python for Beginners'
+print(course)
+# string methods can be choose from list with the use of the . operator
+# i.e. insert string name. will pull up a list of string related functions
+print(course.upper())
+# print string in lower case
+print(course.lower())
+# find index
+print(course.find('P'))
+print(course.find('Beginners'))
+print(course.replace('Beginners', 'Absolute Beginners'))
+# in method
+print('Python'in course)
